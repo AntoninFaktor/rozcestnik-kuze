e86acaaf772524f2aea604e8e171e2c8252eaa55
AntoninFaktor <167860844+AntoninFaktor@users.noreply.github.com>
2025-01-15 11:59:41 +0100

    Committing first files of DB

---
+++ leather-db-app/app/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+            <p id="footer">V případě zájmu o zařazení dalšího výrobce zašlete odkaz na email: 
+                <a href="mailto:info@brasnarstvi-krach.cz" className="email-link"> info@brasnarstvi-krach.cz</a>.
+                 Zařazení není zpoplatněno ani na něj není nárok. Pořadí je generováno náhodně.</p></footer>
+    )
+}
