547fee443c905fb90fbd82fed49fe58538850db9
AntoninFaktor <167860844+AntoninFaktor@users.noreply.github.com>
2025-01-16 20:51:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-base: '/rozcestnik-kuze/'
+  plugins: [react()],
+  base: "/rozcestnik-kuze/",
   })
