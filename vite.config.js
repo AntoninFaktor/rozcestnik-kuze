0a913e0507880a910688c9194f6622c80ef6ce73
AntoninFaktor <167860844+AntoninFaktor@users.noreply.github.com>
2025-01-16 20:17:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	build: {
-	  // generate .vite/manifest.json in outDir
-	  manifest: true,
-	  rollupOptions: {
-		// overwrite default .html entry
-		input: '/index.html',
-	  },
-	},
+	plugins: [
+		react()
+	]
   })
